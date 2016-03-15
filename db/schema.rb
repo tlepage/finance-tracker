94be2945ca95f65628ea4e86cb4a506cd8f7999f
Tom LePage <thomaslepage@live.com>
2016-03-15 07:41:57 +0000

    added styling and various other features

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315062938) do
+ActiveRecord::Schema.define(version: 20160315071343) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 20160315062938) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
