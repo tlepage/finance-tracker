47b0172feff28357694bbdf0274b320f56537ce9
Tom LePage <thomaslepage@live.com>
2016-03-15 07:49:45 +0000

    Added friendships model, view, and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315071343) do
+ActiveRecord::Schema.define(version: 20160315074629) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
